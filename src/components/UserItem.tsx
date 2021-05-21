2b3342feaab6019ab6cd28282f351abfc1d2a090
折田幸夫 <3204704@gmail.com>
2021-05-21 20:27:59 +0900

    emotion×chakura

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,5 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, propNames } from "@chakra-ui/react";
 import React from "react";
+import styled from "@emotion/styled";
+
 type Props = {
   user: {
     name: string;
@@ -8,12 +10,19 @@ type Props = {
   };
 };
 
+const Wrapper = styled.div`
+  display: flex;
+`;
+
 // TODO component style
 const UserItem: React.VFC<Props> = ({ user }) => {
   return (
     <>
-      <Box bg="#EDF2F7" w="100%" p={4} color="white">
-        {user.name}
+      <Box bg="#EDF2F7" color="black" fontWeight="bold" w="100%" p={4}>
+        <Wrapper>
+          <img src={user.image}></img>
+          <p>{user.name}</p>
+        </Wrapper>
       </Box>
     </>
   );
